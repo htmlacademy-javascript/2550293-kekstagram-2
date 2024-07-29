1d59382e78db4a60ed456af085477f7383a0be22
Keks <keks@htmlacademy.ru>
2024-07-29 18:17:06 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/createMiniatures.js
@@ -0,0 +1,41 @@
+import {photos} from './data.js';
+
+const photoData = photos.map(({ url, likes, comments, description }) => ({
+  url,
+  likes,
+  comments: comments.length,
+  description
+}));
+
+function createMiniatures() {
+  //Почему-то commit никак не реагировал на изменения мною файла index.html.
+  //Поэтому создал div в js. Было ли так задуманно или нет, не знаю
+  const div = document.createElement('div');
+  div.className = 'pictures';
+  document.body.insertBefore(div, document.body.firstChild);
+
+  const template = document.querySelector('#picture');
+  const templateContent = template.content;
+  const fragment = document.createDocumentFragment();
+
+  photoData.forEach(({ url, likes, comments, description }) => {
+    const pictureElement = document.importNode(templateContent, true);
+    const imgElement = pictureElement.querySelector('.picture__img');
+    const likesElement = pictureElement.querySelector('.picture__likes');
+    const commentsElement = pictureElement.querySelector('.picture__comments');
+
+    imgElement.src = url;
+    imgElement.alt = description;
+    likesElement.textContent = likes;
+    commentsElement.textContent = comments;
+
+    fragment.appendChild(pictureElement);
+  });
+
+  // Добавляем все элементы из фрагмента в div
+  div.appendChild(fragment);
+}
+
+const createMiniature = createMiniatures();
+
+export {createMiniature};
---
+++ 7/js/main.js
@@ -0,0 +1,5 @@
+import {photos} from './data.js';
+import {createMiniature} from './createMiniatures.js';
+// eslint-disable-next-line no-console
+console.log(JSON.stringify(photos, null, 2));
+createMiniature();
