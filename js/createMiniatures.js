ba1b505eb8d1ded6090d7a6fd3b4ecb012b0b6a3
Kemyar <5088918@gmail.com>
2024-07-29 20:52:50 +0500

    Странности

--- js/createMiniatures.js
+++ js/createMiniatures.js
@@ -10,6 +10,7 @@ const photoData = photos.map(({ url, likes, comments, description }) => ({
 function createMiniatures() {
   //Почему-то commit никак не реагировал на изменения мною файла index.html.
   //Поэтому создал div в js. Было ли так задуманно или нет, не знаю
+  //Не знаю, в итоге он обновиться или нет, по пул реквесту пустота. Странно это все, VSCode не видит или не хочет видеть
   const div = document.createElement('div');
   div.className = 'pictures';
   document.body.insertBefore(div, document.body.firstChild);
