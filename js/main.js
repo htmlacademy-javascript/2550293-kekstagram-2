95a56dd0a0f0a2fa67f5a2592f4723173ada91d1
Kemyar <5088918@gmail.com>
2024-07-31 16:34:39 +0500

    fixssss

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-import { createPhoto} from './data.js';
+import { createPhotos} from './data.js';
 import { createMiniatures } from './create-miniatures.js';
-/*global createPhotos*/
 const photos = createPhotos();
 createMiniatures(photos);
