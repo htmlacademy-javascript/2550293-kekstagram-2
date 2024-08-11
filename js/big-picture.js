d14f6c868e622e2933e0966c61711c0e7b058400
Kemyar <5088918@gmail.com>
2024-08-11 17:54:55 +0500

    Готово

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,3 +1,4 @@
+//2
 import './create-miniatures.js';
 import { isEscapeKey } from './util.js';
 
@@ -11,16 +12,19 @@ const photoDescription = document.querySelector('.social__caption');
 //Global
 const body = document.querySelector('body');
 //Вернемся позже
-const counterComments = document.querySelector('.social__comment-count');
 const totalCountElement = document.querySelector('.social__comment-total-count');
 const shownCountElement = document.querySelector('.social__comment-shown-count');
+const commentTemplate = document.querySelector('#comment-template').content.querySelector('.social__comment');
+const loadMoreButton = document.querySelector('.comments-loader');
+const DEFAULT_COMMENTS_COUNT = 5;
+const commentsContainer = document.querySelector('.social__comments');
 
 const updateCommentCount = (shownCount, totalCount) => {
   shownCountElement.textContent = shownCount;
   totalCountElement.textContent = totalCount;
 };
+
 const renderComment = ({ avatar, name, message }) => {
-  const commentTemplate = document.querySelector('#comment-template').content.querySelector('.social__comment');
   const commentElement = commentTemplate.cloneNode(true);
 
   commentElement.querySelector('.social__picture').src = avatar;
@@ -31,58 +35,46 @@ const renderComment = ({ avatar, name, message }) => {
 };
 
 const renderComments = (comments) => {
-  const commentsFragment = document.createDocumentFragment();
-  const initialComments = comments.slice(0, 5);
-  const remainingComments = comments.slice(5);
+  let commentsCount = 0;
 
-  updateCommentCount(initialComments.length, comments.length);
-
-  initialComments.forEach((comment) => {
-    const commentElement = renderComment(comment);
-    commentsFragment.appendChild(commentElement);
-  });
-
-  const commentsContainer = document.querySelector('.social__comments');
-  commentsContainer.innerHTML = ''; // Очистка старых комментариев
-  commentsContainer.appendChild(commentsFragment);
-  const loadMoreButton = document.querySelector('.comments-loader');
-
-  if (remainingComments.length > 0) {
-    loadMoreButton.style.display = '';
-    let currentIndex = 5;
-    loadMoreButton.addEventListener('click', () => {
-      const remainingCount = comments.length - currentIndex;
-
-      if (remainingCount > 0) {
-        // Определяем, сколько комментариев нужно отобразить
-        const commentsToShow = Math.min(remainingCount, 5);
-
-        const nextComments = comments.slice(currentIndex, currentIndex + commentsToShow);
-        nextComments.forEach((comment) => {
-          const commentElement = renderComment(comment);
-          commentsFragment.appendChild(commentElement);
-        });
-        commentsContainer.appendChild(commentsFragment);
-        const totalShownComments = currentIndex + commentsToShow;
-        updateCommentCount(totalShownComments, comments.length);
-        currentIndex += commentsToShow;
-
-
-        if (currentIndex >= comments.length) {
-          loadMoreButton.style.display = 'none';
-        }
-      }
-    });
-  } else {
-    loadMoreButton.style.display = 'none';
+  function checkCommentsCount () {
+    if (commentsCount === comments.length) {
+      loadMoreButton.classList.add('hidden');
+      loadMoreButton.removeEventListener('click', onLoadMoreClick);
+    } else {
+      loadMoreButton.classList.remove('hidden');
+      loadMoreButton.addEventListener('click', onLoadMoreClick);
+    }
   }
 
-};
+  function addComments () {
+    const startIndex = commentsCount;
+    commentsCount = Math.min(comments.length, commentsCount + DEFAULT_COMMENTS_COUNT);
 
+    const newComments = comments.slice(startIndex, commentsCount);
+    const commentsFragment = document.createDocumentFragment();
+
+    updateCommentCount(commentsCount, comments.length);
+
+    newComments.forEach((comment) => {
+      const commentElement = renderComment(comment);
+      commentsFragment.appendChild(commentElement);
+    });
+
+    commentsContainer.appendChild(commentsFragment);
+    checkCommentsCount();
+  }
+
+  function onLoadMoreClick () {
+    addComments();
+  }
+
+  commentsContainer.innerHTML = '';
+  addComments();
+};
 
 const openBigPicture = (photo) => {
   bigPicture.classList.remove('hidden');
-  counterComments.classList.remove('hidden');
   body.classList.add('modal-open');
 
   bigPictureImg.src = photo.url;
@@ -90,28 +82,24 @@ const openBigPicture = (photo) => {
   photoDescription.textContent = photo.description;
 
   renderComments(photo.comments);
-  document.addEventListener('keydown', onEscClick);
+  document.addEventListener('keydown', onEscKeydown);
   closeButton.addEventListener('click', onButtonClick);
-
 };
 
 const closeBigPicture = () => {
   bigPicture.classList.add('hidden');
   body.classList.remove('modal-open');
-  document.removeEventListener('click', onButtonClick);
+  document.removeEventListener('keydown', onEscKeydown);
 };
 
 function onButtonClick () {
-  if (closeButton) {
-    closeBigPicture();
-  }
+  closeBigPicture();
 }
 
-function onEscClick (evt) {
+function onEscKeydown (evt) {
   if (isEscapeKey(evt)) {
     closeBigPicture();
   }
 }
 
 export { openBigPicture };
-
