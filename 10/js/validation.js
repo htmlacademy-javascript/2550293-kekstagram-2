8bd602dcc88b6c2233a9173b3b4af0957b80168f
Keks <keks@htmlacademy.ru>
2024-08-15 16:16:44 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/validation.js
+++ 10/js/validation.js
@@ -27,11 +27,19 @@
 Реализуйте логику проверки так, чтобы, как минимум, она срабатывала при попытке отправить форму и не давала этого сделать,
 если форма заполнена не по правилам. При желании, реализуйте проверки сразу при вводе значения в поле.
 */
-const inputHeshtags = document.querySelector('.text__hashtags');
+
+const HashtagsRules = {
+  MAX_LENGTH: 20,
+  MAX_AMOUNT: 5
+};
+
+const DESCRIPTION_MAX_LENGTH = 140;
+
 const imgUploadForm = document.querySelector('.img-upload__form');
+const inputHashtags = document.querySelector('.text__hashtags');
 const textDescription = document.querySelector('.text__description');
 
-const pristinHeshtags = new Pristine(imgUploadForm, {
+const pristine = new Pristine(imgUploadForm, {
   classTo: 'img-upload__field-wrapper',
   errorClass: 'form__item--invalid',
   errorTextParent: 'img-upload__field-wrapper',
@@ -39,79 +47,55 @@ const pristinHeshtags = new Pristine(imgUploadForm, {
   errorTextClass: 'form__item--invalid'
 });
 
-function validHashtagMaxLength(value) {
-  return value.split(' ').filter(Boolean).every((hashtag) => hashtag.length <= 20);
-}
+const getHashtagsFromString = (value) =>
+  value.split(' ').filter(Boolean);
 
-function validHashtagStartWith(value) {
-  return value.split(' ').filter(Boolean).every((hashtag) => /^#/.test(hashtag));
-}
+const validateHashtagMaxLength = (value) =>
+  getHashtagsFromString(value).every((hashtag) => hashtag.length <= HashtagsRules.MAX_LENGTH);
 
-function validHashtagSymbols(value) {
-  if (!validHashtagMaxLength(value) || !validUniqueHashtags(value)) {
-    return true;
-  }
+const validateHashtagStartWith = (value) =>
+  getHashtagsFromString(value).every((hashtag) => /^#/.test(hashtag));
 
-  return value.split(' ').filter(Boolean).every((hashtag) => {
-    if (!validHashtagStartWith(hashtag)) {
-      return true; // Игнорируем, если хештег не начинается с #
-    } else if (hashtag.length === 1 && hashtag === '#') {
-      return true; // Разрешаем ввод только символа #
-    } else {
-      const hashtagPart = hashtag.slice(1);
-      return /^[a-zа-я0-9]*$/i.test(hashtagPart);
-    }
-  });
-}
-
-function validHashtagMinLength(value) {
-  return value.split(' ').filter(Boolean).every((hashtag) => {
-    if (!hashtag.startsWith('#')) {
-      return true; // Игнорируем, если хештег не начинается с #
-    }
-    const hashtagPart = hashtag.slice(1).trim();
-    return hashtagPart.length > 0;
-  });
-}
-
-function validMaxHashtags(value) {
-  const hashtags = value.split(' ').filter(Boolean);
-  return hashtags.length <= 5;
-}
-
-function validUniqueHashtags(value) {
-  const hashtags = value.split(' ').filter(Boolean).map((hashtag) => hashtag.toLowerCase());
+const validateUniqueHashtags = (value) => {
+  const hashtags = getHashtagsFromString(value).map((hashtag) => hashtag.toLowerCase());
   const uniqueHashtags = new Set(hashtags);
   return uniqueHashtags.size === hashtags.length;
+};
+
+const validateHashtagSymbols = (value) => getHashtagsFromString(value).every((hashtag) => {
+  const hashtagPart = hashtag.slice(1);
+  return /^[a-zа-я0-9]*$/i.test(hashtagPart);
 }
+);
+
+const validateHashtagMinLength = (value) =>
+  getHashtagsFromString(value).every((hashtag) => {
+    const hashtagPart = hashtag.slice(1);
+    // .trim();
+    return hashtagPart.length > 0;
+  });
+
+const validateMaxHashtags = (value) => {
+  const hashtags = getHashtagsFromString(value);
+  return hashtags.length <= HashtagsRules.MAX_AMOUNT;
+};
 
 
-pristinHeshtags.addValidator(inputHeshtags, validHashtagMaxLength, 'Максимальное колличество символов не должно превышать 20');
-pristinHeshtags.addValidator(inputHeshtags, validHashtagStartWith, 'Хештег должен начинаться с символа "#" ');
-pristinHeshtags.addValidator(inputHeshtags, validHashtagSymbols, 'Хештег должен состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д. ');
-pristinHeshtags.addValidator(inputHeshtags, validHashtagMinLength, 'Минимальное колличество символов должно быть больше, чем  1');
-pristinHeshtags.addValidator(inputHeshtags, validMaxHashtags, 'Максимальное колличество хештегов не должно превышать 5');
-pristinHeshtags.addValidator(inputHeshtags, validUniqueHashtags, 'Хештеги не могут повторяться');
+pristine.addValidator(inputHashtags, validateMaxHashtags, `Максимальное количество хештегов не должно превышать ${HashtagsRules.MAX_AMOUNT}`, 1, true);
+pristine.addValidator(inputHashtags, validateHashtagMaxLength, `Максимальное количество символов не должно превышать ${HashtagsRules.MAX_LENGTH}`, 2, true);
+pristine.addValidator(inputHashtags, validateHashtagStartWith, 'Хештег должен начинаться с символа "#" (решетка)', 3, true);
+pristine.addValidator(inputHashtags, validateHashtagMinLength, 'Хештег не может состоять только из символа "#" (решётка)', 4, true);
+pristine.addValidator(inputHashtags, validateHashtagSymbols, 'Строка после решётки должна состоять только из букв и чисел', 5, true);
+pristine.addValidator(inputHashtags, validateUniqueHashtags, 'Хештеги не могут повторяться', 6, true);
+pristine.addValidator(textDescription, validTextDescriptionMaxLength, `Максимальное количество символов не должно превышать ${DESCRIPTION_MAX_LENGTH}`);
 
-const pristinTextDescription = new Pristine(textDescription, {
-  classTo: 'img-upload__field-wrapper',
-  errorClass: 'form__item--invalid',
-  errorTextParent: 'img-upload__field-wrapper',
-  errorTextTag: 'div',
-  errorTextClass: 'form__item--invalid'
-});
 
 function validTextDescriptionMaxLength(value) {
-  return value.length <= 140;
+  return value.length <= DESCRIPTION_MAX_LENGTH;
 }
 
-pristinTextDescription.addValidator(textDescription, validTextDescriptionMaxLength, 'Максимальное колличество символов не должно превышать 140');
-
 imgUploadForm.addEventListener('submit', (evt) => {
-  const isValidHeshtags = pristinHeshtags.validate();
-  const isValidTextDescription = pristinTextDescription.validate();
-
-  if (!isValidHeshtags || !isValidTextDescription) {
+  if (!pristine.validate()) {
     evt.preventDefault();
   }
 });
