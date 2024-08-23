8eb675e95002a9865bba2ebddeac340d8d339abc
Kemyar <5088918@gmail.com>
2024-08-23 20:05:11 +0500

    Готово

--- js/main.js
+++ js/main.js
@@ -5,29 +5,16 @@ import { closeForm } from './submit-form.js';
 import { setUserFormSubmit } from './submit-form.js';
 import { fetchData } from './server-api.js';
 import { uploadErrorTemplate } from './alerts.js';
+import { setFilters } from './img-filters.js';
+import { imgFilters } from './img-filters.js';
 
 fetchData()
   .then((photos) => {
     createMiniatures(photos);
     initGallery(photos);
+    setFilters(photos);
+    imgFilters.classList.remove('img-filters--inactive');
   })
   .catch(uploadErrorTemplate);
 
 setUserFormSubmit(closeForm);
-
-/*++++++
-№1
-++++++Получение данных:
-  Доработайте модуль для отрисовки фотографий так,
-    чтобы в качестве данных использовались не случайно сгенерированные объекты,
-    а те данные, которые вы загрузите с удалённого сервера:
-      4.1. Загрузка изображений от других пользователей производится сразу после открытия страницы
-        с удалённого сервера: https://31.javascript.htmlacademy.pro/kekstagram/data.
-
-
-+++++Добавьте обработку возможных ошибок при загрузке.
-        4.2. Если при загрузке данных с сервера произошла ошибка запроса,
-        нужно показать соответствующее сообщение. Разметку сообщения,
-        которая находится в блоке #data-error внутри шаблона template, нужно разместить перед закрывающим тегом </body>.
-        Сообщение удаляется со страницы через 5 секунд.
-*/
--- js/util.js
+++ js/util.js
@@ -1,2 +1,41 @@
 const isEscapeKey = (evt) => evt.key === 'Escape';
-export { isEscapeKey };
+
+
+function debounce (callback, timeoutDelay) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
+
+function throttle (callback, delayBetweenFrames) {
+  let lastTime = 0;
+
+  return (...rest) => {
+    // Получаем текущую дату в миллисекундах,
+    // чтобы можно было в дальнейшем
+    // вычислять разницу между кадрами
+    const now = new Date();
+
+    // Если время между кадрами больше задержки,
+    // вызываем наш колбэк и перезаписываем lastTime
+    // временем "последнего кадра"
+    if (now - lastTime >= delayBetweenFrames) {
+      callback.apply(this, rest);
+      lastTime = now;
+    }
+  };
+}
+
+export { isEscapeKey, debounce, throttle };
